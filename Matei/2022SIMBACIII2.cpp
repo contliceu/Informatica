460df966f7142351e155a07cf2c6c38fb278ac8e
BuildTools <unconfigured@null.spigotmc.org>
2022-04-01 10:39:17 +0300

    III2:»

---
+++ Matei/2022SIMBACIII2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char t[250], *cuv, *x;
+    cin.getline(t,250);
+    x = strtok(t, " ");
+    cuv = x;
+    do{
+        if(!strcmp(cuv,x)){
+            cuv = strtok(NULL, "; ");
+            cout<<cuv<<" ";
+        }
+        cuv = strtok(NULL, " ");
+    }while(cuv);
+}
